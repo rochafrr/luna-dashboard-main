621105bf82ec27f99c3ad59a1eeed6989843fab0
Fábio Rodrigues <88008767+rochafrr@users.noreply.github.com>
2022-11-05 11:08:16 -0300

    Update CardSection.js

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -7,7 +7,7 @@ export  class CardSection extends Component {
                 
                 <section className="row m-3 mb-0" style={{ marginTop: ' 2px !important' }}>
                     <div className="card text-white text-center  m-3"
-                        style={{ width: "13rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
+                        style={{ width: "17rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
                         <div className="card-body">
                             <h6 className="card-title" style={{ fontFamily: 'NHaasGroteskDSPro-65Md' }}>Current Price</h6>
                             <p className="card-text fw-bold fs-5" style={{ color: "#006A89" }}>
@@ -18,18 +18,9 @@ export  class CardSection extends Component {
                     
                     
                     
-                  <div className="card text-white text-center  m-3"
-                        style={{ width: "16.5rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
-                        <div className="card-body">
-                            <h6 className="card-title" style={{ fontFamily: 'NHaasGroteskDSPro-65Md' }}>Total Supply</h6>
-                            <p className="card-text fw-bold fs-5" style={{ color: "#006A89" }}>
-                                {parseFloat(this.props.Supply).toLocaleString('pt-br', {minimumFractionDigits: 2})}
-                            </p>
-                        </div>
-                    </div>
-
+                
                     <div className="card text-white text-center  m-3"
-                        style={{ width: "13rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
+                        style={{ width: "17rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
                         <div className="card-body">
                             <h6 className="card-title" style={{ fontFamily: 'NHaasGroteskDSPro-65Md' }}>Positive Sentiments </h6>
                             <p className="card-text fw-bold fs-5" style={{ color: "#006A89" }}>
@@ -38,7 +29,7 @@ export  class CardSection extends Component {
                         </div>
                     </div>
                     <div className="card text-white text-center  m-3"
-                        style={{ width: "13rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
+                        style={{ width: "17rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
                         <div className="card-body">
                             <h6 className="card-title" style={{ fontFamily: 'NHaasGroteskDSPro-65Md' }}> High 24Hrs </h6>
                             <p className="card-text fw-bold fs-5" style={{ color: "rgb(51, 255, 0) " }}>
@@ -47,7 +38,7 @@ export  class CardSection extends Component {
                         </div>
                     </div>
                     <div className="card text-white text-center  m-3"
-                        style={{ width: "13rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
+                        style={{ width: "17rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
                         <div className="card-body">
                             <h6 className="card-title" style={{ fontFamily: 'NHaasGroteskDSPro-65Md' }}> Low 24Hrs </h6>
                             <p className="card-text fw-bold fs-5" style={{ color: 'rgb(255, 32, 32)' }}>
