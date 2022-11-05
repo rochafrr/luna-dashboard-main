c2711b959274353eee4874e6548be42e1b8c3568
Fábio Rodrigues <88008767+rochafrr@users.noreply.github.com>
2022-11-05 10:53:39 -0300

    Update CardSection.js

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -18,12 +18,12 @@ export  class CardSection extends Component {
                     
                     
                     
-                    <div className="card text-white text-center  m-3"
-                        style={{ width: "15.5rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
+                  <div className="card text-white text-center  m-3"
+                        style={{ width: "16.5rem", backgroundColor: "rgb(43, 43, 43)", marginTop: "0px !important" }}>
                         <div className="card-body">
                             <h6 className="card-title" style={{ fontFamily: 'NHaasGroteskDSPro-65Md' }}>Total Supply</h6>
                             <p className="card-text fw-bold fs-5" style={{ color: "#006A89" }}>
-                                {this.props.Supply}
+                                {parseFloat(this.props.Supply).toLocaleString('pt-br', {minimumFractionDigits: 2})}
                             </p>
                         </div>
                     </div>
